2e50d82d5a2deef34529412ac96bfbe3028e589b
Daisuke Takahashi <takahashi_daisuke@cyberagent.co.jp>
2019-05-28 19:22:09 +0900

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
 	fmt.Println(HelloWorld())
 }
 
-// HelloWorld is a function that returns a string containing "hello world".
+// HelloWorld is a function that returns a string "hello world".
 func HelloWorld() string {
 	return "hello world"
 }
